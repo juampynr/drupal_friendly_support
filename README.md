b5dad9bfb79738c9022429e4b943684b2487bfaf
Juampy <juampy72@gmail.com>
2014-02-28 01:35:27 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Friendly Contact - A proof of concept to add authentication to an existing route
+
+Based on http://previousnext.com.au/blog/dynamic-routes-drupal-8-routesubscriber
+
+INSTALLATION
+============
+
+Simply enable the module. Then open /contact in your browser and expect to see the
+popup form requiring username and password.
---
+++ lib/Drupal/friendly_support/Routing/FriendlySupportRouteSubscriber.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Makes it impossible for clients to make a complain.
+ */
+namespace Drupal\friendly_support\Routing;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Listens to the dynamic route events.
+ */
+class FriendlySupportRouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * Alters existing routes for a specific collection.
+   *
+   * @param \Symfony\Component\Routing\RouteCollection $collection
+   *   The route collection for adding routes.
+   * @param string $provider
+   *   The provider these routes belong to. For dynamically added routes, the
+   *   provider name will be 'dynamic_routes'.
+   */
+  public function alterRoutes(RouteCollection $collection, $provider) {
+    // Find the route we want to alter
+    if ($provider == 'contact') {
+      // Load the route, set authentication and add it again.
+      $route = $collection->get('contact.site_page');
+      $route->setOption('_auth', array('basic_auth'));
+      $route->setRequirement('_user_is_logged_in', 'TRUE');
+      $collection->add('contact.site_page', $route);
+    }
+  }
+
+}
